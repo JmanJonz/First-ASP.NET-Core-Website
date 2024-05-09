aba0a79ef84ecea9b0c0c7ddd07478fbf0625279
JmanJonz <wjssaan@gmail.com>
2024-05-08 20:51:44 -0600

    loading in data dynamically using razor page syntax

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,14 +8,17 @@ namespace WebApplication1.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public JsonFileProductService ProductService;
+    public IEnumerable<Product>? Products { get; private set;}
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
     {
         _logger = logger;
+        ProductService = productService;
     }
-
+// when someone gets this page what do you want it to do
     public void OnGet()
     {
-
+        Products = ProductService.GetProducts();
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6049d6308be8a11d16899550f3f6d5b6eefbcf74")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
